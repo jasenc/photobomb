36ddc72065d9b93c8d4f9b80a970c0ce2c27ef87
jasenc <jasen.c8@gmail.com>
2015-07-03 13:54:44 -0700

    added edit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,16 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post = Post.create(post_params)
+    redirect_to(posts_path(@posts))
+  end
+
   def show
     @post = Post.find(params[:id])
   end
