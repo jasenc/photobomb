8d407fe7f6448a4ee3db1ea2f593067390a41279
jasenc <jasen.c8@gmail.com>
2015-07-03 14:02:25 -0700

    added delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,10 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
@@ -22,8 +26,10 @@ class PostsController < ApplicationController
     redirect_to(posts_path(@posts))
   end
 
-  def show
+  def destroy
     @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
